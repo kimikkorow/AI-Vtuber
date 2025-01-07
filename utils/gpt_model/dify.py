aa0b2012bb48d14825ec00aaa8f86e8f0864bf67
ikaros <327209194@qq.com>
2025-01-07 19:53:31 +0800

    新增：弹幕监听脚本支持视频号 弹幕接入

--- utils/gpt_model/dify.py
+++ utils/gpt_model/dify.py
@@ -4,6 +4,7 @@ import traceback
 from urllib.parse import urljoin
 from loguru import logger
 
+from utils.common import Common
 
 class Dify:
     def __init__(self, data: dict):
@@ -13,6 +14,14 @@ class Dify:
 
         # logger.debug(self.config_data)
 
+        self.common = Common()
+
+    def replace_variables(self, text, variables):
+        import re
+
+        for key, value in variables.items():
+            text = re.sub(f'{{{{{key}}}}}', value, text)
+        return text
 
     def get_resp(self, data: dict):
         """请求对应接口，获取返回值
@@ -47,7 +56,7 @@ class Dify:
                 response = requests.request("POST", API_URL, headers=headers, json=data_json)
                 resp_json = json.loads(response.content)
                 
-                logger.debug(f"resp_json={resp_json}")
+                logger.debug(f"[dify] resp_json={resp_json}")
 
                 if "answer" in resp_json:
                     resp_content = resp_json["answer"]
@@ -56,11 +65,49 @@ class Dify:
                     if self.config_data["history_enable"]:
                         self.conversation_id = resp_json["conversation_id"]
                 else:
-                    logger.error(f"获取LLM返回失败。{resp_json}")
+                    logger.error(f"[dify] 获取LLM返回失败。{resp_json}")
                     return None
 
                 return resp_content
-            
+            elif self.config_data["type"] == "工作流":
+                API_URL = urljoin(self.config_data["api_ip_port"], '/v1/workflows/run')
+
+                variables = {
+                    "cur_time": self.common.get_bj_time(0),
+                    "comment": data['prompt'],
+                }
+
+                custom_params = self.replace_variables(self.config_data['custom_params'], variables)
+                custom_params = json.loads(custom_params)
+
+                data_json = {
+                    "inputs": custom_params,
+                    # 阻塞模式
+                    "response_mode": "blocking",
+                    # 用户名是否区分 视情况而定，暂时为了稳定性统一
+                    "user": "test"
+                }
+                logger.debug(f"[dify] data_json={data_json}")
+                headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': f'Bearer {self.config_data["api_key"]}'
+                }
+                
+                response = requests.request("POST", API_URL, headers=headers, json=data_json)
+                resp_json = json.loads(response.content)
+                
+                logger.debug(f"[dify] resp_json={resp_json}")
+
+                if "data" in resp_json:
+                    if "outputs" in resp_json["data"]:
+                        resp_content_dict = resp_json["data"]["outputs"]
+                        if "text" in resp_content_dict:
+                            resp_content = resp_content_dict["text"]
+                else:
+                    logger.error(f"[dify] 获取LLM返回失败。{resp_json}")
+                    return None
+
+                return resp_content
         except Exception as e:
             logger.error(traceback.format_exc())
 
